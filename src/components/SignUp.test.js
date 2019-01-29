bdd9084746f29b21ca02dfc41d16540837913604
Tomas Doherty <tomasdoh1@gmail.com>
2019-01-29 14:17:56 +0000

    Fixing SignUp test

--- src/components/SignUp.test.js
+++ src/components/SignUp.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from './App';
+import SignUp from './SignUp';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
@@ -7,7 +7,7 @@ configure({ adapter: new Adapter() });
 describe('Sign up form', () => {
 
   it('renders the sign up form without crashing', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = shallow(<SignUp />);
 
   })
 });
