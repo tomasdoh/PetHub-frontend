100ff4a139bdd0ba19eaf579b858c2c14130d5e8
elly <elly.chan.mda@gmail.com>
2019-01-29 12:42:31 +0000

    add component PetsView, extract from PetsListing

---
+++ src/components/PetsView.jsx
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import { Container, Dimmer, Loader, Divider, Card } from 'semantic-ui-react';
+
+class PetsView extends Component {
+
+  render () {
+    return ( pet
+      <Container>
+        <Divider hidden section />
+          {results.length
+             ? <Card.Group itemsPerRow={2}>
+               {results.map(pet => {
+                 return <Card
+                 fluid key={pet.id}
+                 image={pet.picture}
+                 header={pet.name}
+                 description={pet.description}
+                 meta={pet.tag}
+                 />
+                 })}
+             </Card.Group>
+             : <Container textAlign='center'>No Pets found.</Container>
+          }
+        <Divider section />
+
+      </Container>
+     )
+  }
+}
+
+export default PetsView;
+
+
+// {results.map(pet => 
+//     <li key={pet.id}>
+//       <a href={pet.name}>{pet.name}</a>
+//     </li>
+//     )}
