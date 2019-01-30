0483a7bd7e0d86541e36a3c70840d521d8c03924
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-30 13:40:27 +0000

    URL for heroku

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,10 +1,10 @@
 //API URLs for Production
-// export const SHORT_URL = "pet-hub-api.herokuapp.com";
-// export const WS_SHORT_URL = 'pet-hub-api.herokuapp.com/cable';
+export const SHORT_URL = "pet-hub-api.herokuapp.com";
+export const WS_SHORT_URL = 'pet-hub-api.herokuapp.com/cable';
 
 //API URL for Local/Development
-export const SHORT_URL = 'localhost:3001';
-export const WS_SHORT_URL = 'localhost:3001/cable';
+// export const SHORT_URL = 'localhost:3001';
+// export const WS_SHORT_URL = 'localhost:3001/cable';
 
 export const URL = "http://" + SHORT_URL;
 export const WS_URL = "wss://" + WS_SHORT_URL;
