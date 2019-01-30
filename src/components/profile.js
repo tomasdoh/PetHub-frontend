a9b43b8a17f45e04892380d4473c5298ceab5b75
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-30 14:41:38 +0000

    adding pets view inside profile component

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,2 +1,16 @@
 import React, {Component} from 'react';
 import {Container, Image} from 'semantic-ui-react';
+import PetsView from "./PetsView";
+
+class Profile extends Component {
+
+  render () {
+    const {results} = this.state;
+    return (
+      <Container>
+        <PetsView data={results}/>
+      </Container>
+    )
+  }
+
+}
