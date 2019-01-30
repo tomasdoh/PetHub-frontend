96b78444d7266c96d631f0d0758cbca17b53cd83
Tomas Doherty <tomasdoh1@gmail.com>
2019-01-30 15:45:18 +0000

    Putting previous data in Update form placeholder

--- src/components/UpdatePet.js
+++ src/components/UpdatePet.js
@@ -7,13 +7,15 @@ import FileBase64 from 'react-file-base64';
 class UpdatePet extends Component {
   constructor() {
     super();
-    this.state = {};
-    this.getPet = this.getPet.bind(this)
+    this.state = { pet: [], };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
-    this.getPet(this.props.match.params.id);
+    return window.fetch(URL + `/pets/${this.props.match.params.id}`)
+      .then(response => response.json())
+      .then(data => this.setState({ pet: data}))
+      .catch(error => console.log(error))
   }
 
   handleSubmit(event) {
@@ -37,16 +39,9 @@ class UpdatePet extends Component {
     this.setState({file: file});
   }
 
-  getPet(id) {
-    fetch(URL +`/pets/${this.props.match.params.id}`, {
-    })
-    .then(pet => this.setState({pet: pet}))
-  }
-
   render() {
-    let {pet} = this.state;
-    return pet
-      ?
+    const {pet} = this.state
+    return (
       <Container className='App'>
         <h1>Update pet</h1>
         <Form onSubmit={this.handleSubmit} className="form">
@@ -54,29 +49,29 @@ class UpdatePet extends Component {
             <Col>
               <FormGroup>
                 <Label for="name">Name</Label>
-                <Input type="text" autoFocus className='form-name' name="name" id="name" placeholder="Name" />
+                <Input type="text" autoFocus className='form-name' name="name" id="name" placeholder={pet.name} />
               </FormGroup>
             </Col>
             <Col>
               <FormGroup>
                 <Label for="owner">Owner</Label>
-                <Input type="text" autoFocus className='pet-owner' name="owner" id="owner" placeholder="Owner" />
+                <Input type="text" autoFocus className='pet-owner' name="owner" id="owner" placeholder={pet.owner} />
               </FormGroup>
             </Col>
           </Row>
           <FormGroup>
             <Label for="description">Description</Label>
-            <Input type="textarea" autoFocus className='pet-description' name="description" id="description" placeholder="Description"/>
+            <Input type="textarea" autoFocus className='pet-description' name="description" id="description" placeholder={pet.description} />
           </FormGroup>
           <FormGroup>
             <Label for="exampleFile">Picture</Label>
-            <FileBase64 multiple={false} onDone={this.getFile.bind(this)}/>
+            <FileBase64 multiple={false} onDone={this.getFile.bind(this)} placeholder={pet.fileBase64}/>
             <FormText color="muted">
             </FormText>
           </FormGroup>
           <FormGroup>
             <Label for="exampleSelect">Status</Label>
-            <Input type="select" name="tag" id="exampleTag">
+            <Input type="select" name="tag" id="exampleTag" placeholder={pet.tag}>
               <option>Lost</option>
               <option>Found</option>
               <option>Reunited</option>
@@ -91,9 +86,7 @@ class UpdatePet extends Component {
           <Button color="dark" className='button' style={{marginTop: '2rem'}} block>Report</Button>
         </Form>
       </Container>
-      : <div>
-      Sorry, you can't update this pet
-      </div>
+    )
   }
 }
 
